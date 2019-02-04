a32646e7e9af2d27b9b81124fa346eb40319a326
jiho lee <jh001007@gmail.com>
2019-02-04 15:45:11 +0900

    Create 1-16.scm

---
+++ 1/1-16.scm
@@ -0,0 +1,9 @@
+(define (square x) (* x x))
+(define (even? n) (= (remainder n 2) 0))
+(define (fast-expt b n) (fast-expt-iter 1 b n))
+(define (fast-expt-iter curr b n)
+  (cond ((= n 0) curr)
+        ((even? n) (fast-expt-iter curr (square b) (/ n 2)))
+        (else (fast-expt-iter (* curr b) b (- n 1)))))
+
+(display (fast-expt 3 2))
