341ff4741ce43ee3eb16fb3dc1864201169c73b3
jiho lee <jh001007@gmail.com>
2019-02-04 15:55:00 +0900

    Create 1-18.scm

---
+++ 1-18.scm
@@ -0,0 +1,7 @@
+(define (double x) (* x 2))
+(define (halve x) (/ x 2))
+(define (even? n) (= (remainder n 2) 0))
+(define (mult a b)
+	(cond ((= b 0) 0)
+	      ((even? b) (mult (double a) (halve b)))
+	      (else (+ a (mult a (- b 1))))))
