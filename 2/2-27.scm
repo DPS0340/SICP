74d6f30edaa7535285102abee35a2a7d2f9f29f2
jiho lee <jiholee@kpu.ac.kr>
2019-10-08 07:16:48 +0900

    Create 2-27.scm

---
+++ 2/2-27.scm
@@ -0,0 +1,11 @@
+(define (deep-reverse list)
+  (define empty `())
+  (define (deep-reverse-iter result list1)
+    (if (null? list1)
+        result
+        (if (pair? (car list1))
+            (deep-reverse-iter (cons (reverse (car list1)) result) (cdr list1))
+            (deep-reverse-iter (cons (car list1) result) (cdr list1)))))
+  (deep-reverse-iter empty list))
+
+(deep-reverse (list (list 1 2) (list 3 4)))
