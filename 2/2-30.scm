716c6caa6403ba13af1620072c3c46c80b860bd0
jiho lee <jiholee@kpu.ac.kr>
2019-10-09 14:57:18 +0900

    Create 2-30.scm

---
+++ 2/2-30.scm
@@ -0,0 +1,20 @@
+(define (map proc items)
+       (if (null? items)
+           nil
+           (cons (proc (car items))
+                 (map proc (cdr items)))))
+
+(define (square x) (* x x))
+
+(define (square-tree-recursive tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (square tree))
+        (else (cons (square-tree-recursive (car tree))
+                    (square-tree-recursive (cdr tree))))))
+
+(define (square-tree-map tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree-map sub-tree)
+             (square sub-tree)))
+       tree))
