d04defb79237101aa1a16c8e09c5c88d7f5b3be3
jiho lee <jiholee@kpu.ac.kr>
2019-10-10 19:10:36 +0900

    Create 2-39.scm

---
+++ 2/2-39.scm
@@ -0,0 +1,21 @@
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (reverse sequence)
+  (fold-right (lambda (x y) (list y x)) nil sequence))
+
+(define (reverse-fl sequence)
+  (fold-left (lambda (x y) (list y x)) nil sequence))
+
+(reverse-fl (list 1 2 3 4 5))
