aff82c2805573aa0318bde50d522e73c313950bd
jiho lee <jiholee@kpu.ac.kr>
2019-10-11 15:16:42 +0900

    Update 2-42.scm

--- 2/2-42.scm
+++ 2/2-42.scm
@@ -1,4 +1,32 @@
+(define (map proc items)
+       (if (null? items)
+           nil
+           (cons (proc (car items))
+                 (map proc (cdr items)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (filter f seq)
+  (if (null? seq)
+      nil
+      (if (f (car seq))
+          (cons (car seq) (filter f (cdr seq)))
+          (filter f (cdr seq)))
+      )
+  )
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
 (define (queens board-size)
+  (define (abs x)
+    (if (< x 0)
+        (* x -1)
+        x))
   (define (location x y)
     (list x y))
   (define (get-x loc)
@@ -14,7 +42,7 @@
         (let (
               (my (car positions))
               (opposite (car (cdr positions))))
-        (if (or (= (get-x my) (get-x opposite)) (= (get-y my) (get-y opposite)) (= (/ (get-y my) (get-x my)) (/ (get-y opposite) (get-x opposite))))
+        (if (or (= (get-x my) (get-x opposite)) (= (get-y my) (get-y opposite)) (= (abs (- (get-y my) (get-y opposite))) (abs (- (get-x my) (get-x opposite)))))
             #f
             (safe? (- k 1) (cdr positions))))))
   (define (queen-cols k)
@@ -30,5 +58,5 @@
           (queen-cols (- k 1))))))
   (queen-cols board-size))
 
-(queens 2) ; {{{1 2} {2 1}}}
-; it works
+(queens 3) ; {{{1 3} {3 2} {1 1}} {{3 3} {1 2} {3 1}}}
+; it works well
